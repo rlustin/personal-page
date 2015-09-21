4e04b07922cda532187bcd3ef8165f37bf9d050d
Raphaël Lustin <raphael@lustin.fr>
2015-09-21 15:18:55 +0200

    Update U Pro experience with ending date.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,5 @@
 'use strict';
 
-function timeSince(year, month, day) {
-  var date = new Date(year, month - 1, day);
-  var diff = Math.floor((new Date()).getTime() - date.getTime());
-  var dayInSec = 1000 * 60 * 60 * 24;
-  var monthInSec = dayInSec * 30;
-  var yearInSec = dayInSec * 365;
-
-  var years = Math.floor(diff / yearInSec);
-  var months = Math.floor((diff - years * yearInSec) / monthInSec);
-
-  if (years === 0) {
-    return months + ' mois';
-  }
-  var yearString = years > 1 ? ' ans' : ' an';
-  if (months === 0) {
-    return years + yearString;
-  }
-
-  return years + yearString + ' et ' + months + ' mois';
-}
-
-$('.date-since').each(function () {
-  $(this).html('(' + timeSince($(this).data('year'), $(this).data('month'), $(this).data('day')) + ')');
-});
-
 $('.nav a[href^="#"]').click(function () {
   $('.navbar-toggle').click();
   $('html, body').animate({
